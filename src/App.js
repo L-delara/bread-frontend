7e8fac335792f6d7060b20423bb468ab843e3375
L-delara <ligia.delara15@gmail.com>
2023-06-15 15:55:50 -0400

    Fixed errors, is functional

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Bread from "./Components/Bread";
+import New from "./Components/New";
 
 function App() {
-  const [breads, setBreads] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const URL = `${process.env.REACT_APP_BACKEND_URI}/breads`;
-      const response = await fetch(URL);
-      const data = await response.json();
-      setBreads(data);
-    };
-
-    fetchData();
-  }, []);
-
-  const display = breads.map((bread) => {
-    return (
-      <div key={bread._id}>
-        <p>{bread.name}</p>
-      </div>
-    );
-  });
-
   return (
     <div>
-      <h1>BreadCRUD</h1>
-      {display}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bread/:id" element={<Bread />} />
+          <Route path="/new" element={<New />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Bread.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Bread() {
+  const [bread, setBread] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = `${process.env.REACT_APP_BACKEND_URI}/breads/${id}`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      setBread(data);
+    };
+
+    fetchData();
+  }, [id]);
+
+  return (
+    <div>
+      <h1>{bread.name}</h1>
+      <img src={bread.image} alt={bread.name} height={300} />
+    </div>
+  );
+}
+
+export default Bread;
